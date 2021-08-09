c7d2f7e04bdb710a0ef0a5e473a45654e69afd48
chase <cjopsahl99@gmail.com>
2021-08-09 15:14:26 -0500

    added help command/fixed clear

--- automatic-goggles.py
+++ automatic-goggles.py
@@ -3,6 +3,7 @@ import shutil
 from c_color import *
 from os import system as s
 from c_manager import *
+from sys import platform
 
 # TODO: Have configuration file that allows for nodes to be preconfigured. 
 def main():
@@ -44,15 +45,17 @@ def menu():
             m.close(node)
         elif spl_cmd[0] == "export":
             m.export_history("session_info.txt")
+        elif spl_cmd[0] == "help":
+            aghelp()
         elif spl_cmd[0] == 'clear':
-            s('clear')
+            clear()
         elif spl_cmd[0] in ['quit', 'exit']:
             break
         else:
             print("Invalid command!")
 
 
-def help():
+def aghelp():
     """Display possible commands to user
         connect "name" '127.0.0.1' 1234 ConnType.REV_SHELL
         connect "name" '65.123.3.1' 1337 ConnType.TCP_CONN
@@ -64,7 +67,29 @@ def help():
         shell <node> 
         help
     """
-    pass
+    print('''
+    connect <name> <ip> <port> <connection_type> - Create a node for specific connectoin type.
+        * Connection Types:
+            TCP_CONN  -> Directly connect to remote shell.
+            REV_SHELL -> Set up listener to recieve reverse shell connection.
+            (i.e.) connect node_name 192.168.0.3 1337 TCP_CONN
+                   connect node_name 0.0.0.0 1234 REV_SHELL
+    
+    list            - List All Nodes
+    status <node>   - Get the status of a specific node (DEAD, LISTENING, CONNECTED)
+    info <node>     - Get basic information of a given node connection. (Hostname, current user, os version)
+    shell <node>    - Drop into a shell on the given node and run commands manually.
+    help            - Display this help information.
+    close <node>    - Close a connection on a given node.
+    export          - Export a text file containing every command run, with its output.
+    quit, exit      - Close automatic goggles and kill connections.
+    ''')
+
+def clear():
+    if "win" in platform:
+        s("cls")
+    else:
+        s("clear")
 
 if __name__ == '__main__':
     main()
