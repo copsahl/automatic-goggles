e5a3149de08ffc0b6afdeeec48d55bfbf96f89a1
chase <cjopsahl99@gmail.com>
2021-08-11 15:20:50 -0500

    Clean up home dir

---
+++ src/c_color.py
@@ -0,0 +1,8 @@
+class Color:
+    BLUE = '\033[1;34m'
+    RED = '\033[1;31m'
+    CYAN = '\033[1;36m'
+    YELLOW = '\033[0;33m'
+    GREEN = '\033[0;32m'
+    PURPLE = '\033[0;35m'
+    END = '\033[0;0m'
